bdfda9c4c60699f44499d35521214130f9b96b9e
VinayakGardi <vinayakgardi7@gmail.com>
2023-02-13 14:16:02 +0530

    next commit

--- src/Sorting.java
+++ src/Sorting.java
@@ -2,22 +2,26 @@ import java.util.Arrays;
 
 public class Sorting {
     public static void main(String[] args) {
-        int nums[]={4,525,1,553,66,32};
+        int nums[]={1,2,3,4,5};
         bubble(nums);
+        System.out.println(Arrays.toString(nums));
 
     }
 
     static void  bubble(int nums[]) {
-        for(int i=0;i< nums.length;i++){
-            for(int j=0;j< nums.length-1;j++) {
-                if(nums[j]>nums[j+1]){
-                    int temp= nums[i];
-                    nums[i]=nums[j];
-                    nums[j]=temp;
+        boolean swapped = false;
+        for(int i=0;i<nums.length;i++) {
+            for(int j=1;j< nums.length-i;j++) {
+                if(nums[j] < nums[j-1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j-1];
+                    nums[j-1] = temp;
                 }
             }
+            if(!swapped) {
+                break;           // if no swap is done in the 1st pass then no need check for further passes so break from this loop
+            }
         }
-        System.out.println(Arrays.toString(nums));
 
     }
 }
